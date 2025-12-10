776fc5098aacf55ee6917ebe2bf49d34d166157d
Müslime Kayar <muslimekayar2121@gmail.com>
2025-12-10 11:05:49 +0300

    feat: Implement new phishing detection rules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,8 @@
 import { Logger, Module } from '@nestjs/common';
-import { MaliciousAppModule } from './modules/malicious-app/malicious-app.module';
 import { PhishingModule } from './modules/phishing/phishing.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { SchedulerModule } from './modules/scheduler/scheduler.module';
+
 
 @Module({
     imports: [
@@ -33,9 +32,8 @@ import { SchedulerModule } from './modules/scheduler/scheduler.module';
             },
             inject: [ConfigService],
         }),
-        MaliciousAppModule,
+
         PhishingModule,
-        SchedulerModule,
     ],
     controllers: [],
     providers: [],

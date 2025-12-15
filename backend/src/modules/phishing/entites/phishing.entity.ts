453c9df111ead8b35ad703c965748fafc8465ad0
Müslime Kayar <muslimekayar2121@gmail.com>
2025-12-15 15:45:58 +0300

    feat(entity): Refactor PhishingEntity imports and add missing Index decorator for source

--- backend/src/modules/phishing/entites/phishing.entity.ts
+++ backend/src/modules/phishing/entites/phishing.entity.ts
@@ -1,15 +1,27 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  Index,
+} from 'typeorm';
 
 @Entity({ name: 'phishing' })
 export class PhishingEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Index()
   @Column({ unique: true })
   url: string;
 
+  @Index()
   @Column()
   source: string;
+
+  // @Index()
+  // @Column()
+  // target: string;
 
   @CreateDateColumn()
   createdAt: Date;
